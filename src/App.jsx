e6c5dd685b6c36531e16b169f220c944e0eb2e50
Alexandrav21 <alexandra.vladu@thetrainline.com>
2024-11-23 23:03:48 +0000

    moved existing react code to this repo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Weather from './Weather'
 
 function App() {
 
@@ -6,10 +7,13 @@ function App() {
   return (
     <>
       <div>
+        <h1> Weather App </h1>
+        <Weather />
         <a href="https://vitejs.dev" target="_blank"> </a>
       </div>
       <p className="read-the-docs">
-        This project was created by Alexandra Vladu 
+        This project was created by Alexandra Vladu and it is {" "}
+        <a href="https://github.com/alexandravladu/react-weather-app" target="_blank">open-source</a>
       </p>
     </>
   )
---
+++ src/Weather.jsx
@@ -0,0 +1,70 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+
+const Weather = () => {
+  const [weather, setWeather] = useState({});
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [message, setMessage] = useState();
+
+  const showTemperature = (response) => {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3a94f3778290bfeee61278505dbbe51d&units=metric`;
+    axios.get(url).then(showTemperature);
+    setMessage(`The weather in ${city} is: `);
+  };
+
+  const updateCity = (event) => {
+    setCity(event.target.value);
+  };
+
+  let form = (
+    <form onSubmit={handleSubmit}>
+      <input type="search" onChange={updateCity} placeholder="Search..." />
+      <input type="submit" value="Search" />
+    </form>
+  );
+
+  if (loaded) {
+    return (
+      <div>
+        {form}
+        <br />
+        <h2>{message} </h2>
+        <ul>
+          <li>
+            Temperature: <strong> {Math.round(weather.temperature)}°C </strong>
+          </li>
+          <li>
+            Description: <strong> {weather.description} </strong>
+          </li>
+          <li>
+            Humidity: <strong> {weather.humidity}% </strong>{" "}
+          </li>
+          <li>
+            Wind: <strong> {weather.wind}km/h </strong>{" "}
+          </li>
+          <li>
+            <img src={weather.icon} alt={weather.description} />
+          </li>
+        </ul>
+      </div>
+    );
+  } else {
+    return form;
+  }
+};
+
+export default Weather;
