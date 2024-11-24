055aa412eacdf9ec03765a8dc5c2e3a8ebeff7da
Alexandrav21 <alexandra.vladu@thetrainline.com>
2024-11-24 10:51:40 +0000

    added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,18 @@ function App() {
 
 
   return (
-    <>
-      <div>
+    <div className='App'>
+      <div className='container'>
         <h1> Weather App </h1>
         <Weather />
+        <button className='btn btn-primary'>Button </button>
         <a href="https://vitejs.dev" target="_blank"> </a>
       </div>
       <p className="read-the-docs">
         This project was created by Alexandra Vladu and it is {" "}
         <a href="https://github.com/alexandravladu/react-weather-app" target="_blank">open-source</a>
       </p>
-    </>
+    </div>
   )
 }
 
