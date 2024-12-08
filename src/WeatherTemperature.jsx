43e756555909aba217a08e2a58cb46c822b5bdf1
Alexandrav21 <alexandra.vladu@thetrainline.com>
2024-12-08 12:40:58 +0000

    remove the temp conversion to show just celsius

--- src/WeatherTemperature.jsx
+++ src/WeatherTemperature.jsx
@@ -1,33 +1,14 @@
 /* eslint-disable react/prop-types */
  
-import React, {useState} from 'react'
+import React from 'react'
 
 const WeatherTemperature = (props) => {
-    const [unit, setUnit] = useState("celsius");
-    const showFahrenheit = (event) =>{
-        event.preventDefault();
-        setUnit("fahrenheit")
-    }
-    const showCelsius = (event) => {
-        event.preventDefault();
-        setUnit("celsius");
-    }
-    if(unit === 'celsius') {
-        return (
-            <div className='WeatherTemperature'>
-                <span className="temperature">{Math.round(props.celsius)}</span>
-                <span className="unit">°C | <a href='/' onClick={showFahrenheit}> °F </a></span>
-            </div>
-          );
-    } else {
-        let fahrenheit = (props.celsius * 9/5) + 32;
-        return (
-            <div className='WeatherTemperature'>
-                <span className="temperature">{Math.round(fahrenheit)}</span>
-                <span className="unit"><a href='/' onClick={showCelsius}> °C </a>| °F</span>
-            </div>
-          );
-    }
+    return (
+        <div className='WeatherTemperature'>
+            <span className="temperature">{Math.round(props.celsius)}</span>
+             <span className="unit">°C </span>
+        </div>
+        );
 }
 
 export default WeatherTemperature;
