a605140a0dde69583877f9614dcde40f4667a454
Alexandrav21 <alexandra.vladu@thetrainline.com>
2024-12-08 12:55:28 +0000

    fix alignment issue

--- src/WeatherInfo.jsx
+++ src/WeatherInfo.jsx
@@ -1,37 +1,35 @@
 /* eslint-disable react/prop-types */
- 
 import React from 'react';
 import FormattedDate from './FormattedDate';
 import WeatherTemperature from './WeatherTemperature';
-
+import './WeatherInfo.css';
 
 const WeatherInfo = (props) => {
   return (
-    <div className='WeatherInfo'>
-      <h1> {props.data.city} </h1>
+    <div className="WeatherInfo">
+      <h1>{props.data.city}</h1>
       <ul>
-        <li> <FormattedDate date={props.data.date} /> </li>
-        <li className="text-capitalize"> {props.data.description}</li>
+        <li><FormattedDate date={props.data.date} /></li>
+        <li className="text-capitalize">{props.data.description}</li>
       </ul>
       <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <img src={props.data.icon} alt={props.data.description} className="float-left" />
-            <div className="float-left">
-              <WeatherTemperature celsius={props.data.temperature} />
-            </div>
-          </div>
+        <div className="col-6 d-flex align-items-center">
+          <img
+            src={props.data.icon}
+            alt={props.data.description}
+            className="weather-icon me-3"
+          />
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
         <div className="col-6">
           <ul>
-            <li>Humidity: {props.data.humidity}</li>
-            <li>Wind: {props.data.wind}</li>
+            <li>Humidity: {props.data.humidity}%</li>
+            <li>Wind: {props.data.wind} km/h</li>
           </ul>
         </div>
       </div>
     </div>
-  )
+  );
 };
-
 
 export default WeatherInfo;
